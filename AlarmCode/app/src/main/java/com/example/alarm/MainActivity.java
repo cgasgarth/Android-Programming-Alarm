42391159f194600268d70230095c8f4b99dc9a7e
cgasgarth <64235119+cgasgarth@users.noreply.github.com>
2022-03-18 11:02:44 -0400

    Basic homescreen implemented

--- AlarmCode/app/src/main/java/com/example/alarm/MainActivity.java
+++ AlarmCode/app/src/main/java/com/example/alarm/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.alarm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TimePicker tp = findViewById(R.id.timePicker1);
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+            Calendar c=Calendar.getInstance();
+
+            int hr = tp.getHour();
+            int m = tp.getMinute();
+
+
+            c.set(Calendar.HOUR_OF_DAY,hr);
+            c.set(Calendar.MINUTE,m);
+        //Add logic to set date of the calendar object and get time in milliseconds like we did earlier for setting the alarm
+        }
     }
 }

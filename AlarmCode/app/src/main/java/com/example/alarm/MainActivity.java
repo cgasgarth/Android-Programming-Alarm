3ae31d8d53991507386eb59e23a116ee42d1a0cc
cgasgarth <64235119+cgasgarth@users.noreply.github.com>
2022-03-18 11:23:26 -0400

    Spinner implemented

--- AlarmCode/app/src/main/java/com/example/alarm/MainActivity.java
+++ AlarmCode/app/src/main/java/com/example/alarm/MainActivity.java
@@ -2,17 +2,35 @@ package com.example.alarm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ClipData;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TimePicker;
 
 import java.util.Calendar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.times_array, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
+
 
         TimePicker tp = findViewById(R.id.timePicker1);
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
@@ -26,5 +44,19 @@ public class MainActivity extends AppCompatActivity {
             c.set(Calendar.MINUTE,m);
         //Add logic to set date of the calendar object and get time in milliseconds like we did earlier for setting the alarm
         }
+    }
+
+    public void onItemSelected(AdapterView<?> parent, View view,
+                               int pos, long id) {
+
+        // An item was selected. You can retrieve the selected item using
+        // parent.getItemAtPosition(pos)
+         String item = (String) parent.getItemAtPosition(pos);
+         Log.i("item selected is", item);
+
+    }
+
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Another interface callback
     }
 }
